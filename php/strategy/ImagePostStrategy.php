5ded2a1258d4f2fdaa477dcb757fa7d2fd615fe1
Beatriz Reis <abeatrizreis1@gmail.com>
2024-11-25 22:41:04 -0300

    mudança pra diferenciar os posts

--- php/strategy/ImagePostStrategy.php
+++ php/strategy/ImagePostStrategy.php
@@ -3,7 +3,7 @@ require_once __DIR__ . '/../strategy/PostStrategy.php';
 
 class ImagePostStrategy implements PostStrategy {
     public function display(Post $post) {
-        $html = "<h3>Imagem do Post</h3>";
+        $html = "<h3>Imagem</h3>";
         $html .= "<p>ID do Post: " . htmlspecialchars($post->getId()) . "</p>";
         $html .= "<p>Conteúdo: " . htmlspecialchars($post->getConteudo()) . "</p>";  // Exibe o conteúdo (imagem URL)
         $html .= "<img src='" . htmlspecialchars($post->getConteudo()) . "' alt='Imagem do Post' style='max-width: 100%;'>";
--- php/strategy/TextPostStrategy.php
+++ php/strategy/TextPostStrategy.php
@@ -1,11 +1,12 @@
 <?php
 require_once __DIR__ . '/../strategy/PostStrategy.php';
 require_once __DIR__ . '/../factory/TextPost.php';
+require_once __DIR__ . '/../factory/Post.php';
 
 
 class TextPostStrategy implements PostStrategy {
     public function display(Post $post) {
-        $html = "<h3>Texto do Post</h3>";
+        $html = "<h3>Texto</h3>";
         $html .= "<p>ID do Post: " . htmlspecialchars($post->getId()) . "</p>";
         $html .= "<p>Conteúdo: " . htmlspecialchars($post->getConteudo()) . "</p>";  // Exibe o conteúdo (texto)
         
